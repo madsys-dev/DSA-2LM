094dfbed0ff077a93fc2a809947e14b6f9ed76ef
LRL52 <525687841@qq.com>
2024-09-30 17:09:18 +0800

    update sysctl code for dsa_sync_mode

--- linux-5.15.19-memtis/kernel/sysctl.c
+++ linux-5.15.19-memtis/kernel/sysctl.c
@@ -118,6 +118,7 @@ extern int limit_chans;
 extern int dsa_copy_threshold;
 extern int use_concur_for_htmm;
 extern int dsa_async_mode;
+extern int dsa_sync_mode;
 
 /* Constants used for minimum and  maximum */
 #ifdef CONFIG_LOCKUP_DETECTOR
@@ -2962,6 +2963,15 @@ static struct ctl_table vm_table[] = {
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_ONE,
+	},
+	{
+		.procname	= "dsa_sync_mode",
+		.data		= &dsa_sync_mode,
+		.maxlen		= sizeof(dsa_sync_mode),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_THREE,
 	},
 	 {
 		.procname	= "hugetlb_shm_group",
